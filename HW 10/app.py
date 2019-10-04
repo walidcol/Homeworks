94484db9b901c5a1dd4d08aeee8f47bfa9171bb5
walid <47761160+walidcol@users.noreply.github.com>
2019-10-04 16:11:39 -0400

    finishing app.py

--- HW 10/app.py
+++ HW 10/app.py
@@ -4,3 +4,36 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+
+# Flask Setup
+app = Flask(__name__)
+
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
+def convert_to_dict(query_result, label):
+    data = []
+    for record in query_result:
+        data.append({'date': record[0], label: record[1]})
+    return data
+
+
+def get_most_recent_date():
+    recent_date = session.query(Measurement).\
+        order_by(Measurement.date.desc()).limit(1)
+
+    for date in recent_date:
+        most_recent_date = date.date
+
+    return dt.datetime.strptime(most_recent_date, "%Y-%m-%d")
